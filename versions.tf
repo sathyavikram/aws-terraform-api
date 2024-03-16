7d4d30cc32fbf209420ac371859c49e3cf5d3d2c
Sathya Vikram <2120744+sathyavikram@users.noreply.github.com>
2024-03-16 11:04:54 -0400

    Added terraform cloud configuration

--- versions.tf
+++ versions.tf
@@ -1,3 +1,11 @@
 terraform {
   required_version = ">= 1.7.5"
+
+  cloud {
+    ## same organization name created in terraform cloud account
+    organization = "learn-and-create"
+    workspaces {
+      tags = ["aws-terraform-api"]
+    }
+  }
 }
