2ebb623881ab2d315e77c10194be199d45a39713
Sathya Vikram <2120744+sathyavikram@users.noreply.github.com>
2024-03-20 06:17:05 -0400

    added aws provider

--- versions.tf
+++ versions.tf
@@ -2,10 +2,22 @@ terraform {
   required_version = ">= 1.7.5"
 
   cloud {
-    ## same organization name created in terraform cloud account
+    # Organization name you created in your terraform cloud account.
+    # This is different for each account. Use same name you created in your account
     organization = "learn-and-create"
     workspaces {
       tags = ["aws-terraform-api"]
     }
   }
+
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.41.0"
+    }
+  }
+}
+
+provider "aws" {
+  region = "us-east-1"
 }
